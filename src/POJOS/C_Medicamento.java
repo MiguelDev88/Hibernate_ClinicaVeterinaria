1a6c4750b1b1dadce38c08851bf23b1a75659e4b
MiguelDev88 <mpereirafern@gmail.com>
2017-02-28 20:10:50 +0100

    Edited main classes, still work to do

--- src/POJOS/C_Medicamento.java
+++ src/POJOS/C_Medicamento.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package POJOS;
 
 
-// @author Migu
+// @author Miguel
 
 public class C_Medicamento {
+    
+    private String nombre;
+    private String tipo;
+    private String principioActivo;
+    private String dosis;
 
 }
