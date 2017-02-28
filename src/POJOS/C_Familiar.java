93b75c5ec262333338d5b8d22f727d0fbd1cd934
MiguelDev88 <mpereirafern@gmail.com>
2017-02-28 19:30:51 +0100

    Main classes added, starting DB relationship

---
+++ src/POJOS/C_Familiar.java
@@ -0,0 +1,13 @@
+package POJOS;
+
+import java.util.List;
+
+
+// @author Miguel
+
+public class C_Familiar extends C_Persona {
+    
+    private List mascotas;
+    
+
+}
---
+++ src/POJOS/C_Persona.java
@@ -0,0 +1,18 @@
+package POJOS;
+
+
+// @author Miguel
+
+public class C_Persona {
+    
+    private String dni;
+    private String nombre;
+    private String telefono;
+    
+    
+    public C_Persona (){}
+    
+    
+    
+
+}
