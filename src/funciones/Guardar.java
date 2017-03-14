86f7d08fd93fa847361b5e629addc9cff2063ddf
a15carlosmpf <a15carlosmpf@W10D-I4E20.ateis.local>
2017-03-14 13:59:38 +0100

    More work

---
+++ src/funciones/Guardar.java
@@ -0,0 +1,31 @@
+package funciones;
+import POJOS.*;
+import hibernate_clinicaveterinaria.HibernateUtil;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Guardar {
+    
+    public static void guardarAnimal (C_Animal animal) {
+        
+        
+        Session sesion=HibernateUtil.getSession();
+        
+        C_Persona familiar = (C_Familiar)sesion.createQuery("FROM POJOS.C_Persona f WHERE f.dni='"+animal.getFamiliar().getDni()+"'").uniqueResult();
+
+        if(familiar != null)
+            animal.setFamiliar(familiar);
+        
+        System.out.println("voy a guardar");
+        System.out.println("save");
+        sesion.beginTransaction();
+        sesion.save(animal);
+        sesion.getTransaction().commit();
+        sesion.close();
+        
+    }
+    
+}
